e4b36fdff1230eb08b42d7e74dcbb0a0cd9a8257
pipupapu <127877505+pipupapu@users.noreply.github.com>
2023-05-26 10:04:19 +0300

    changes

--- code/codetoUI.js
+++ code/codetoUI.js
@@ -9,16 +9,16 @@ document.getElementById("confirm").onclick=function(){
     if(name.length > 0 & name2.length > 0 & allergy.length > 0){
         //if all info, green
         document.getElementById("ok").style.display="block";
-        document.getElementById("error").style.display="none";
+        document.getElementById("error").style.display="none"; 
     }
     else{
         //if all textfields are not filled, background is red
         document.getElementById("error").style.display="block";
         document.getElementById("ok").style.display="none";
     }
-    //name + allergy + date + local time
-    document.getElementById("result", "date").innerHTML+=
-    " Nimi: " + name + " " + name2 + "<br>" + " Ruoka-aineallergiat: " + allergy + "<br>" + date + "<br>"; 
-    return false;
+        //name + allergy + date + local time
+        document.getElementById("result", "date").innerHTML+=
+        " Nimi: " + name + " " + name2 + "<br>" + " Ruoka-aineallergiat: " + allergy + "<br>" + date + "<br>"; 
+        return false;    
 }
 
